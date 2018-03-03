cab837eb8933cd802c9109b2f35dd01895256f74
kimshuye <suntisuk.go@gmail.com>
2018-03-04 00:13:15 +0700

    set authService state status passing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
 
+import { Router } from '@angular/router';
+
+import { AuthService } from './providers/auth.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  isLoggedIn : boolean;
+  displayname;
+  email;
+
+  constructor(
+    public authService:AuthService,
+    private router:Router
+  ){
+    this.authService.af.authState.subscribe(auth =>{
+      if(auth == null){
+        // not logged in
+        this.isLoggedIn = false;
+        this.displayname = '';
+        this.email = '';
+        this.router.navigate(['login']);
+      }else{
+        // logged in
+        this.isLoggedIn = true;
+        this.displayname = auth.displayName;
+        this.email = auth.email;
+        this.router.navigate(['']);
+      }
+    });
+  }
 }
