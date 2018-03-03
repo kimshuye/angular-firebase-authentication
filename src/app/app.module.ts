02b12763dca79e47dc6159f35298ea4e8bfb16dc
kimshuye <suntisuk.go@gmail.com>
2018-03-03 19:33:51 +0700

    add firebase service and loginpage component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// firebase configuration
+import { AngularFireModule } from 'angularfire2';
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
 
+// services
+import { AuthService } from './providers/auth.service';
+import { LoginpageComponent } from './component/loginpage/loginpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginpageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase,'angular-firebase-authentication'),
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
